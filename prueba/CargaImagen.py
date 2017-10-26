9c8977890e17027577766feafa6f0f1e3e7acc4d
lumerlino <merlinolucia@hotmail.com>
2017-10-26 19:08:23 -0300

    hay que ponerle game over

--- prueba/CargaImagen.py
+++ prueba/CargaImagen.py
@@ -1,36 +1,16 @@
 import pygame
 
-#gordo1=pygame.image.load('imagenes/Running1.PNG')
-#gordo2=pygame.image.load('imagenes/Running2.PNG')
-#gordo3=pygame.image.load('imagenes/Running3.PNG')
-#gordo4=pygame.image.load('imagenes/Running4.PNG')
-manzana=pygame.image.load('manzana.PNG')
-spritemanzana=pygame.sprite.Sprite()
-spritemanzana.image= manzana
-spritemanzana.rect=manzana.get_rect()
-spritemanzana.rect.top=50
-spritemanzana.rect.left=50
-
-cereza=pygame.image.load('cereza.PNG')
-ensalada=pygame.image.load('ensalada.PNG')
-pochoclo=pygame.image.load('pochoclos.PNG')
-panqueques=pygame.image.load('panqueques.PNG')
-torta=pygame.image.load('torta.PNG')
+gordo1=pygame.image.load('Imagenes/chavon1.PNG')
+gordo2=pygame.image.load('Imagenes/chavon2.PNG')
+gordo3=pygame.image.load('Imagenes/chavon3.PNG')
+gordo4=pygame.image.load('Imagenes/chavon4.PNG')
+manzana=pygame.image.load('Imagenes/manzana.PNG')
+cereza=pygame.image.load('Imagenes/cereza.PNG')
+ensalada=pygame.image.load('Imagenes/ensalada.PNG')
+pochoclo=pygame.image.load('Imagenes/pochoclos.PNG')
+panqueques=pygame.image.load('Imagenes/panqueques.PNG')
+torta=pygame.image.load('Imagenes/torta.PNG')
 #fondo=pygame.image.load('imagenes/manzana.PNG')
 #gordo_list=[gordo1,gordo2,gordo3,gordo4]
-all_sprite=[manzana,cereza,ensalada,pochoclo,panqueques,torta]
 
-gordo1=pygame.image.load('chavon1.PNG')
-gordo2=pygame.image.load('chavon2.PNG')
-gordo3=pygame.image.load('chavon3.PNG')
-gordo4=pygame.image.load('chavon4.PNG')
-comida1=pygame.image.load('manzana.PNG')
-comida2=pygame.image.load('manzana.PNG')
-fondo=pygame.image.load('manzana.PNG')
-menu=pygame.image.load_extended("menu2.png")
-conluz=pygame.image.load("letra1.png")
-sinluz=pygame.image.load("letra2.png")
-conluz1=pygame.image.load("letra7.png")
-sinluz1=pygame.image.load("letra8.png")
-gordo_list=[gordo1,gordo2,gordo3,gordo4]
-all_sprite=[comida1,comida2,gordo1,gordo2,gordo3,gordo4,fondo,menu,conluz,conluz1,sinluz,sinluz1]
+listade_sprites = [manzana,cereza,ensalada,pochoclo,panqueques,torta]
